b7ac0b054ac8a6de447c5099ec3037433cd39f79
Tomas Barrios <tomasbarrios@gmail.com>
2023-05-15 13:08:44 -0400

    foto y logo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,15 +12,15 @@ export default function Index() {
             <div className="absolute inset-0">
               <img
                 className="h-full w-full object-cover"
-                src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031-34fc371ac0d6.jpg"
-                alt="Sonic Youth On Stage"
+                src="https://mpost.io/wp-content/uploads/image-73-5.jpg"
+                alt="Baby dreams"
               />
               <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
             </div>
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-yellow-500 drop-shadow-md">
-                  Sueños comunes
+                  Lista de deseos
                 </span>
               </h1>
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
