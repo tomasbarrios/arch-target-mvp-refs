4e8920d351d8696c47aa29c084383d13a9d29fe9
Tomas Barrios <tomasbarrios@gmail.com>
2023-08-01 11:22:42 -0400

    problem with task, deleting

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -74,49 +74,49 @@ async function seed() {
   //    * Added by me
   //    */
 
-  const tasks = [
-    {
-      title: "My First Post",
-      body: `
-    # This is my first task
+  // const tasks = [
+  //   {
+  //     title: "My First Post",
+  //     body: `
+  //   # This is my first task
 
-    Isn't it great?
-        `.trim(),
-      noteId: note.id,
-    },
-    {
-      title: "A Mixtape I Made Just For You",
-      body: `
-    # 90s Mixtape
+  //   Isn't it great?
+  //       `.trim(),
+  //     noteId: note.id,
+  //   },
+  //   {
+  //     title: "A Mixtape I Made Just For You",
+  //     body: `
+  //   # 90s Mixtape
 
-    - I wish (Skee-Lo)
-    - This Is How We Do It (Montell Jordan)
-    - Everlong (Foo Fighters)
-    - Ms. Jackson (Outkast)
-    - Interstate Love Song (Stone Temple Pilots)
-    - Killing Me Softly With His Song (Fugees, Ms. Lauryn Hill)
-    - Just a Friend (Biz Markie)
-    - The Man Who Sold The World (Nirvana)
-    - Semi-Charmed Life (Third Eye Blind)
-    - ...Baby One More Time (Britney Spears)
-    - Better Man (Pearl Jam)
-    - It's All Coming Back to Me Now (Céline Dion)
-    - This Kiss (Faith Hill)
-    - Fly Away (Lenny Kravits)
-    - Scar Tissue (Red Hot Chili Peppers)
-    - Santa Monica (Everclear)
-    - C'mon N' Ride it (Quad City DJ's)
-        `.trim(),
-    },
-  ];
+  //   - I wish (Skee-Lo)
+  //   - This Is How We Do It (Montell Jordan)
+  //   - Everlong (Foo Fighters)
+  //   - Ms. Jackson (Outkast)
+  //   - Interstate Love Song (Stone Temple Pilots)
+  //   - Killing Me Softly With His Song (Fugees, Ms. Lauryn Hill)
+  //   - Just a Friend (Biz Markie)
+  //   - The Man Who Sold The World (Nirvana)
+  //   - Semi-Charmed Life (Third Eye Blind)
+  //   - ...Baby One More Time (Britney Spears)
+  //   - Better Man (Pearl Jam)
+  //   - It's All Coming Back to Me Now (Céline Dion)
+  //   - This Kiss (Faith Hill)
+  //   - Fly Away (Lenny Kravits)
+  //   - Scar Tissue (Red Hot Chili Peppers)
+  //   - Santa Monica (Everclear)
+  //   - C'mon N' Ride it (Quad City DJ's)
+  //       `.trim(),
+  //   },
+  // ];
 
-  for (const task of tasks) {
-    // await prisma.task.upsert({
-    //   where: { title: task.title },
-    // await prisma.task.create({
-    //   data: task,
-    // });
-  }
+  // for (const task of tasks) {
+  // await prisma.task.upsert({
+  //   where: { title: task.title },
+  // await prisma.task.create({
+  //   data: task,
+  // });
+  // }
 
   const wishes = [
     {
